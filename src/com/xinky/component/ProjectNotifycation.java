83682c36db0492166c320b618997079b1baa6548
zhangxinkun <zxk844977406@outlook.com>
2019-05-20 09:15:36 +0800

    remove vote channel

--- src/com/xinky/component/ProjectNotifycation.java
+++ src/com/xinky/component/ProjectNotifycation.java
@@ -1,13 +1,5 @@
 package com.xinky.component;
 
-import java.util.Date;
-
-import org.apache.commons.lang3.time.DateUtils;
-
-import com.intellij.ide.util.PropertiesComponent;
-import com.intellij.notification.Notification;
-import com.intellij.notification.NotificationListener;
-import com.intellij.notification.NotificationType;
 import com.intellij.openapi.components.ProjectComponent;
 import com.intellij.openapi.project.Project;
 
@@ -26,16 +18,19 @@ public class ProjectNotifycation implements ProjectComponent {
 
   @Override
   public void projectOpened() {
-    long lastNotify = PropertiesComponent.getInstance(project).getOrInitLong(KEY_LASTNOTIFY, 0);
-    if (DateUtils.isSameDay(new Date(), new Date(lastNotify))) {
-      return;
-    }
-    final Notification notification = new Notification(
-        "Material Design Dark-Theme",
-        "Material Design Dark-Theme",
-        "<html>If you love my theme plugin, please vote on <a href=\"https://plugins.jetbrains.com/contest/intellij-themes/2019\">IntelliJ Themes Contest 2019</a>. <br/>Really thanks!<html>",
-        NotificationType.INFORMATION, NotificationListener.URL_OPENING_LISTENER);
-    notification.notify(project);
-    PropertiesComponent.getInstance(project).setValue(KEY_LASTNOTIFY, new Date().getTime() + "");
+    // long lastNotify =
+    // PropertiesComponent.getInstance(project).getOrInitLong(KEY_LASTNOTIFY,
+    // 0);
+    // if (DateUtils.isSameDay(new Date(), new Date(lastNotify))) {
+    // return;
+    // }
+    // final Notification notification = new Notification(
+    // "Material Design Dark-Theme",
+    // "Material Design Dark-Theme",
+    // "<html>If you love my theme plugin, please vote on <a href=\"https://plugins.jetbrains.com/contest/intellij-themes/2019\">IntelliJ Themes Contest 2019</a>. <br/>Really thanks!<html>",
+    // NotificationType.INFORMATION, NotificationListener.URL_OPENING_LISTENER);
+    // notification.notify(project);
+    // PropertiesComponent.getInstance(project).setValue(KEY_LASTNOTIFY, new
+    // Date().getTime() + "");
   }
 }
